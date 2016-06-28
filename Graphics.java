512a607371c53daab46510855af2162c36834620
Ilnur <badlog.charli@yandex.ru>
2016-06-28 05:52:20 +0300

    Обновление формы и дальнейшее изучение

--- Graphics.java
+++ Graphics.java
@@ -3,6 +3,14 @@ import javax.swing.*;
 /**
  * Created by Ilnur Khafizov on 28.06.2016.
  */
-public class Graphics {
+public class Graphics extends JFrame {
+    private JButton button1;
 
+    public Graphics()
+    {
+        super("Sosi pisos nooborot");
+        setSize(640, 480);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
--- Main.java
+++ Main.java
@@ -4,10 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Dijkstra d = new Dijkstra();
         //d.transfier();
-        JFrame frame = new JFrame("Алгоритм Де́йкстры");
+        /*JFrame frame = new JFrame("Алгоритм Де́йкстры");
         frame.setSize(640, 480);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-        frame.setLocationRelativeTo(null);
+        frame.setLocationRelativeTo(null);*/
+        Dijkstra grapouny = new Dijkstra();
+        grapouny.setVisible(true);
     }
 }
